5ec996da64a5276ea228bc83b2a680a8e9d6287d
max-de-rooij <54850292+max-de-rooij@users.noreply.github.com>
2025-12-04 18:39:21 +0100

    Update solution.jl

--- day04/solution.jl
+++ day04/solution.jl
@@ -1,58 +1,64 @@
-function read_rolls(file_name)
-    open(file_name) do io
-        locs = CartesianIndex{2}[]
-        for (row,line) in enumerate(readlines(io))
-            cols = findall('@', line)
-            append!(locs, [CartesianIndex(row, col) for col in cols])
-        end
-        locs
+function readsplitlines(input_file)
+    open(input_file, "r") do io
+    split.(readlines(io), "")
     end
 end
 
-function neighbors(index::T, indices::Vector{T}) where T<:CartesianIndex{2}
-    # return the neighbors of index that are in indices
-    Δ = CartesianIndex.([(-1, -1), (-1, 0), (1, 1), (0, -1), (1, 0), (0, 1), (1, -1), (-1, 1)])
-    return indices[indices .∈ Ref(index .+ Δ)]
-end
+function day04_part1(input_file="day04/inputs/day04.txt")
+    lines = readsplitlines(input_file)
+    counter = 0
+    H = length(lines); W = length(lines[1])
+    for i in 1:H, j in 1:W
+        if lines[i][j] == "@"
+            neighbor_count = 0
+            for di in (-1,0,1), dj in (-1,0,1)
+                if di != 0 || dj != 0
+                    ni = i + di; nj = j + dj
+                    if 1 <= ni <= H && 1 <= nj <= W && lines[ni][nj] == "@"
+                        neighbor_count += 1
+                    end
+                end
+            end
 
-function neighborsin(index::T, indices::Vector{T}, n::Int) where T<:CartesianIndex{2}
-    Δ = CartesianIndex.([(-1, -1), (-1, 0), (1, 1), (0, -1), (1, 0), (0, 1), (1, -1), (-1, 1)])
-    count(i -> i ∈ indices, index .+ Δ) .< n
-end
-
-function day04_part1(input_file="day04/inputs/day04.txt") 
-    roll_locations = read_rolls(input_file)
-    count(l -> neighborsin(l, roll_locations, 4), roll_locations)
+            if neighbor_count < 4
+                counter += 1
+            end
+        end
+    end
+    return counter
 end
 
 function day04_part2(input_file="day04/inputs/day04.txt")
-    original_roll_locations = read_rolls(input_file)
-    n_dropped = 1; total_dropped = 0
-    locations_to_check = copy(original_roll_locations)
-    while n_dropped > 0
-        # which to drop
-        drop_ = map(l -> neighborsin(l, original_roll_locations, 4), locations_to_check)
-        rolls_to_drop = locations_to_check[drop_]
-        # find their neighbors
-        new_rolls_to_check = CartesianIndex{2}[]
-        for roll in rolls_to_drop
-            append!(new_rolls_to_check, neighbors(roll, original_roll_locations))
-        end
+    lines = readsplitlines(input_file)
+    counter = 0; removed = 1
+    H = length(lines); W = length(lines[1])
+    indices_to_check = [(i,j) for i in 1:H, j in 1:W][:]
+    while !isempty(indices_to_check)
 
-        # make unique
-        new_rolls_to_check = unique(new_rolls_to_check)
-        # remove the ones to drop
-        new_rolls_to_check = new_rolls_to_check[.!(new_rolls_to_check .∈ Ref(rolls_to_drop))]
-        original_roll_locations = copy(original_roll_locations[.!(original_roll_locations .∈ Ref(rolls_to_drop))])
-        
-        n_dropped = length(rolls_to_drop)
-        locations_to_check = new_rolls_to_check
-        total_dropped += n_dropped
+        i,j = pop!(indices_to_check)
+        if lines[i][j] == "@"
+            # count neighbors
+            neighbors = Tuple{Int,Int}[]
+            for di in (-1, 0, 1), dj in (-1, 0, 1)
+                if di != 0 || dj != 0
+                    ni = i + di
+                    nj = j + dj
+                    if 1 <= ni <= H && 1 <= nj <= W && lines[ni][nj] == "@"
+                        push!(neighbors, (ni, nj))
+                    end
+                end
+            end
+            if length(neighbors) < 4
+                counter += 1
+                lines[i][j] = "x"
+                append!(indices_to_check, neighbors)
+            end
+        end
     end
-    total_dropped
+
+    return counter
 end
 
-
-# get outputs
+# # get outputs
 println("Day 4 - Part 1: $(day04_part1())")
 println("Day 4 - Part 2: $(day04_part2())")
